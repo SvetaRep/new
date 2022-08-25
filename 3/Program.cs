c7cf55d895bf8664cad4dc5abb9ad742210e3127
Svetlana <svetarepalenko@yandex.ru>
2022-08-25 12:48:08 +0300

    done

---
+++ 3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите целое число ");
+Console.ReadLine();
+int a= Convert.ToInt32(Console.ReadLine() );
+if (a%2) 
+{
+    Console.Write(a+ "является целым числом");
+}
+else
+{
+    Console.Write(a+ "не является целым числом");
+}
