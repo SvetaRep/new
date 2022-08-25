608371cba1c9ffe44a9a850a572cd1bab19bc70a
Svetlana <svetarepalenko@yandex.ru>
2022-08-25 12:45:10 +0300

    done

---
+++ 1/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите два целых числа ");
+int a= Convert.ToInt32(Console.ReadLine());
+int b= Convert.ToInt32(Console.ReadLine());
+if (a>b) Console.WriteLine(a+"max");
+else Console.WriteLine(b+"max");
