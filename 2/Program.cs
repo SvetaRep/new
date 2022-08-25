19529779ee24459ddbe69f8b11ada63b1c0d7fe7
Svetlana <svetarepalenko@yandex.ru>
2022-08-25 12:46:52 +0300

    done

---
+++ 2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите три целых числа ");
+
+int a = Convert.ToInt32(Console.ReadLine() );
+int b = Convert.ToInt32(Console.ReadLine() );
+int c = Convert.ToInt32(Console.ReadLine() );
+int max = a;
+if ( max < b ) 
+{
+    max = b ;
+}
+if ( max< c ) 
+{
+    max = c;
+}
+Console.Write( "Максимальное число " + max );
